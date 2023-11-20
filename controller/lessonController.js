ec702f971817b5896eb22e50b93e669539ccf4c1
Felix <felix.bongo@tweeklabs.com>
2023-11-20 16:52:42 +0800

    udpate

--- controller/lessonController.js
+++ controller/lessonController.js
@@ -225,200 +225,202 @@ exports.getLesson = async function(req, res, next) {
 
       res.json(lesson)
 
-    }
-
-    let lessonData = {}
-    let columns = res.app.locals.helpers.getLessonColumns()
-    columns[0] = "v3_id as id"
-
-    if (inputs.slug) {
-      
-      lessonData = (await Lessons.getMysqlProduction(`
-        SELECT ${columns.join(',')}
-        FROM contents 
-        WHERE slug='${encodeURI(inputs.slug)}'
-      `))[0]
-
     } else {
+      let lessonData = {}
+      let columns = res.app.locals.helpers.getLessonColumns()
+      columns[0] = "v3_id as id"
 
-      lessonData = (await Lessons.getMysqlProduction(`
-        SELECT ${columns.join(',')}
-        FROM contents 
-        WHERE v3_id='${encodeURI(inputs.lessonId)}'
-      `))[0]
-    }
+      if (inputs.slug) {
+        
+        lessonData = (await Lessons.getMysqlProduction(`
+          SELECT ${columns.join(',')}
+          FROM contents 
+          WHERE slug='${encodeURI(inputs.slug)}'
+        `))[0]
 
-    if (lessonData && lessonData.slug) {
-
-      lesson = lessonData
-      lesson.introduction = sanitizeHtml(lesson.introduction, sanitizeOptions)
-
-      let userLessons = await Lessons.getMysqlProduction(`Select v3_id, saved, studied, created_at as updatedAt 
-                                    From user_contents 
-                                    WHERE user_id=${userId} 
-                                    AND v3_id='${lesson.id}'
-                                    ORDER BY created_at DESC
-                                    LIMIT ${inputs.limit ? inputs.limit : 10}
-                                `); 
-
-      if (userLessons[0]) {
-        lesson.studied = userLessons[0].studied ? userLessons[0].studied : false
-        lesson.saved = userLessons[0].saved ? userLessons[0].saved : false
       } else {
-        lesson.studied = false
-        lesson.saved = false
+
+        lessonData = (await Lessons.getMysqlProduction(`
+          SELECT ${columns.join(',')}
+          FROM contents 
+          WHERE v3_id='${encodeURI(inputs.lessonId)}'
+        `))[0]
       }
 
-      let lessonRoot = `https://s3contents.chinesepod.com/${
-        lessonData.type === 'extra' ? 'extra/' : ''
-      }${lessonData.id}/${lessonData.hash_code}/`
-      if (lesson.image) {
-        lesson.image = cleanLink(
-          lessonData.image && lessonData.image.startsWith('http')
-            ? lessonData.image
-            : lessonRoot + lessonData.image
-        )
-      }
-      // console.log("dialogue data", lessonData.mp3_dialogue);
-      if (lessonData.mp3_dialogue) {
-        if (lessonData.mp3_dialogue.endsWith('.mp3')) {
-          lesson.mp3_dialogue = cleanLink(
-            lessonData.mp3_dialogue && lessonData.mp3_dialogue.startsWith('http')
-              ? lessonData.mp3_dialogue
-              : lessonRoot + lessonData.mp3_dialogue
-          )
+      if (lessonData && lessonData.slug) {
+
+        lesson = lessonData
+        lesson.introduction = sanitizeHtml(lesson.introduction, sanitizeOptions)
+
+        let userLessons = await Lessons.getMysqlProduction(`Select v3_id, saved, studied, created_at as updatedAt 
+                                      From user_contents 
+                                      WHERE user_id=${userId} 
+                                      AND v3_id='${lesson.id}'
+                                      ORDER BY created_at DESC
+                                      LIMIT ${inputs.limit ? inputs.limit : 10}
+                                  `); 
+
+        if (userLessons[0]) {
+          lesson.studied = userLessons[0].studied ? userLessons[0].studied : false
+          lesson.saved = userLessons[0].saved ? userLessons[0].saved : false
         } else {
-          lesson.mp3_dialogue = '';
+          lesson.studied = false
+          lesson.saved = false
         }
-        
-      }
-      if (lessonData.mp3_public) {
-        if (lessonData.mp3_public.endsWith('.mp3')) {
-          lesson.mp3_public = cleanLink(
-            lessonData.mp3_public && lessonData.mp3_public.startsWith('http')
-              ? lessonData.mp3_public
-              : lessonRoot + lessonData.mp3_public
+
+        let lessonRoot = `https://s3contents.chinesepod.com/${
+          lessonData.type === 'extra' ? 'extra/' : ''
+        }${lessonData.id}/${lessonData.hash_code}/`
+        if (lesson.image) {
+          lesson.image = cleanLink(
+            lessonData.image && lessonData.image.startsWith('http')
+              ? lessonData.image
+              : lessonRoot + lessonData.image
           )
-        } else {
-          lesson.mp3_public = '';
         }
-        
-      }
-      if (lessonData.mp3_private) {
-        if (lessonData.mp3_private.endsWith('.mp3')) {
-          lesson.mp3_private = cleanLink(
-            lessonData.mp3_private && lessonData.mp3_private.startsWith('http')
-              ? lessonData.mp3_private
-              : lessonRoot + lessonData.mp3_private
-          )
-        } else {
-          lesson.mp3_private = '';
-        }
-      }
-      if (lessonData.mp3_thefix) {
-        if (lessonData.mp3_thefix.endsWith('.mp3')) {
-          lesson.mp3_thefix = cleanLink(
-            lessonData.mp3_thefix && lessonData.mp3_thefix.startsWith('http')
-              ? lessonData.mp3_thefix
-              : lessonRoot + lessonData.mp3_thefix
-          )
-        } else {
-          lesson.mp3_thefix = '';
-        }
-      }
-      if (lessonData.pdf1) {
-        if (lessonData.pdf1.endsWith('.pdf')) {
-          lesson.pdf1 = cleanLink(
-            lessonData.pdf1 && lessonData.pdf1.startsWith('http')
-              ? lessonData.pdf1
-              : lessonRoot + lessonData.pdf1
-          )
-        } else {
-          lesson.pdf1 = '';
-        }
-      }
-      if (lessonData.pdf2) {
-        if (lessonData.pdf2.endsWith('.pdf')) {
-          lesson.pdf2 = cleanLink(
-            lessonData.pdf2 && lessonData.pdf2.startsWith('http')
-              ? lessonData.pdf2
-              : lessonRoot + lessonData.pdf2
-          )
-        } else {
-          lesson.pdf2 = '';
-        }
-      }
-
-      lesson.extra = lesson.type === 'extra'
-
-      lesson.sources = (await LessonSources.findQuery({v3_id: lesson.id}))[0]
-
-      if (lesson.sources || lesson.video) {
-        // const access = await sails.helpers.users.getAccessType(inputs.userId)
-
-        let accessInfo = await userService.getAccessTypeAndExpiry(userId)
-
-        let access = accessInfo.type
-
-        const geo = geoip.lookup(req.ip)
-
-        if (!lesson.sources) {
-          lesson.sources = { wistia: {} }
-        }
-
-        if (lesson.sources && lesson.sources.wistia && lesson.video) {
-          lesson.sources.wistia.simplified = lesson.video
-        }
-
-        try {
-          if (lesson.video && lesson.sources) {
-
-            // TODO: FIND RECORDS IN DYNAMODB
-            // CREATE REPOSITORIES LESSONFILES
-
-            // console.log("this", test);
-            const assets = (
-              await LessonFiles.findQuery({id:lesson.id})
-            )[0]
-              
-            // console.log(assets)
-            if (assets && assets.srcVideo) {
-              lesson.sources['hls'] = { simplified: assets.srcVideo }
-              // lesson.sources['mp4'] = { simplified: assets.mp4Urls }
-            }
+        // console.log("dialogue data", lessonData.mp3_dialogue);
+        if (lessonData.mp3_dialogue) {
+          if (lessonData.mp3_dialogue.endsWith('.mp3')) {
+            lesson.mp3_dialogue = cleanLink(
+              lessonData.mp3_dialogue && lessonData.mp3_dialogue.startsWith('http')
+                ? lessonData.mp3_dialogue
+                : lessonRoot + lessonData.mp3_dialogue
+            )
+          } else {
+            lesson.mp3_dialogue = '';
           }
-        } catch (e) {
-          console.log(e)
-          // sails.hooks.bugsnag.notify(e)
-          // sails.log.error(e)
+          
+        }
+        if (lessonData.mp3_public) {
+          if (lessonData.mp3_public.endsWith('.mp3')) {
+            lesson.mp3_public = cleanLink(
+              lessonData.mp3_public && lessonData.mp3_public.startsWith('http')
+                ? lessonData.mp3_public
+                : lessonRoot + lessonData.mp3_public
+            )
+          } else {
+            lesson.mp3_public = '';
+          }
+          
+        }
+        if (lessonData.mp3_private) {
+          if (lessonData.mp3_private.endsWith('.mp3')) {
+            lesson.mp3_private = cleanLink(
+              lessonData.mp3_private && lessonData.mp3_private.startsWith('http')
+                ? lessonData.mp3_private
+                : lessonRoot + lessonData.mp3_private
+            )
+          } else {
+            lesson.mp3_private = '';
+          }
+        }
+        if (lessonData.mp3_thefix) {
+          if (lessonData.mp3_thefix.endsWith('.mp3')) {
+            lesson.mp3_thefix = cleanLink(
+              lessonData.mp3_thefix && lessonData.mp3_thefix.startsWith('http')
+                ? lessonData.mp3_thefix
+                : lessonRoot + lessonData.mp3_thefix
+            )
+          } else {
+            lesson.mp3_thefix = '';
+          }
+        }
+        if (lessonData.pdf1) {
+          if (lessonData.pdf1.endsWith('.pdf')) {
+            lesson.pdf1 = cleanLink(
+              lessonData.pdf1 && lessonData.pdf1.startsWith('http')
+                ? lessonData.pdf1
+                : lessonRoot + lessonData.pdf1
+            )
+          } else {
+            lesson.pdf1 = '';
+          }
+        }
+        if (lessonData.pdf2) {
+          if (lessonData.pdf2.endsWith('.pdf')) {
+            lesson.pdf2 = cleanLink(
+              lessonData.pdf2 && lessonData.pdf2.startsWith('http')
+                ? lessonData.pdf2
+                : lessonRoot + lessonData.pdf2
+            )
+          } else {
+            lesson.pdf2 = '';
+          }
         }
 
-        // check if no HLS / get new hls links
-        let newHls = (await LessonNewSources.findQuery({v3_id:lesson.id }))[0]
+        lesson.extra = lesson.type === 'extra'
 
-        if (newHls) {
-          // set hls link to the new URL
-          lesson.sources['hls'] = { simplified: newHls.src }
+        lesson.sources = (await LessonSources.findQuery({v3_id: lesson.id}))[0]
+
+        if (lesson.sources || lesson.video) {
+          // const access = await sails.helpers.users.getAccessType(inputs.userId)
+
+          let accessInfo = await userService.getAccessTypeAndExpiry(userId)
+
+          let access = accessInfo.type
+
+          const geo = geoip.lookup(req.ip)
+
+          if (!lesson.sources) {
+            lesson.sources = { wistia: {} }
+          }
+
+          if (lesson.sources && lesson.sources.wistia && lesson.video) {
+            lesson.sources.wistia.simplified = lesson.video
+          }
+
+          try {
+            if (lesson.video && lesson.sources) {
+
+              // TODO: FIND RECORDS IN DYNAMODB
+              // CREATE REPOSITORIES LESSONFILES
+
+              // console.log("this", test);
+              const assets = (
+                await LessonFiles.findQuery({id:lesson.id})
+              )[0]
+                
+              // console.log(assets)
+              if (assets && assets.srcVideo) {
+                lesson.sources['hls'] = { simplified: assets.srcVideo }
+                // lesson.sources['mp4'] = { simplified: assets.mp4Urls }
+              }
+            }
+          } catch (e) {
+            console.log(e)
+            // sails.hooks.bugsnag.notify(e)
+            // sails.log.error(e)
+          }
+
+          // check if no HLS / get new hls links
+          let newHls = (await LessonNewSources.findQuery({v3_id:lesson.id }))[0]
+
+          if (newHls) {
+            // set hls link to the new URL
+            lesson.sources['hls'] = { simplified: newHls.src }
+          }
+
+          if (
+            (!geo || geo.country !== 'CN') &&
+            access === 'free' &&
+            (lesson.sources.youtube || lesson.sources.vimeo)
+          ) {
+            delete lesson.sources.wistia
+            delete lesson.sources.hls
+            // delete lesson.sources.mp4
+          }
         }
 
-        if (
-          (!geo || geo.country !== 'CN') &&
-          access === 'free' &&
-          (lesson.sources.youtube || lesson.sources.vimeo)
-        ) {
-          delete lesson.sources.wistia
-          delete lesson.sources.hls
-          // delete lesson.sources.mp4
-        }
+        Lessons.upsert({id:lesson.id}, lesson);
+        await redisClientLesson.set(inputs.slug, JSON.stringify(lesson))
+
+        res.json(lesson)
+      } else {
+        throw 'invalid'
       }
-
-      Lessons.upsert({id:lesson.id}, lesson);
-      await redisClientLesson.set(inputs.slug, JSON.stringify(lesson))
-
-      res.json(lesson)
-    } else {
-      throw 'invalid'
     }
+
+    
     
   }
 }
