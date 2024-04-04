77ea950adfa5d02bef1eb46350dd939a3574ec5d
Felix <felix@chinesepod.com>
2024-04-04 10:52:01 +0800

    update

--- api/android-intro-tutorials.js
+++ api/android-intro-tutorials.js
@@ -1,6 +1,8 @@
 
 exports.fn = async function(req, res, next) {
-    
+  
+  console.log(">>> android intro API")
+  
   res.json({
     success: 1,
     video: 'https://d2arcxjkuir81y.cloudfront.net/assets/HUE0000.HowtoUseChinesePod.Elijah.Introduction/HLS/HUE0000.HowtoUseChinesePod.Elijah.Introduction.m3u8'
