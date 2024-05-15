f1045910dc422e90c0ae26a2e785b6ba96373410
Felix <felix@chinesepod.com>
2024-05-15 11:24:21 +0800

    udpate

--- api/setUserDeleted.js
+++ api/setUserDeleted.js
@@ -4,44 +4,70 @@ let Users = require('../repositories/users')
 
 exports.fn = async function(req, res, next) {
 
-  // console.log(req.params.id, userId)
+  let userIdSession = req.session.userId
   // get user ID
   let userId = req.params.id
 
-  // udpate records
-  if (userId) {
-    let user = (await Users.getUserByIdSQL(userId))[0]
-    if (user) {
+  if (!userIdSession || userId != userIdSession) {
+    res.json({
+      status: false,
+      message: "Permission denied"
+    });
+    // console.log('asd');
 
-      let updateQuery = "update user_site_links set usertype_id=20 where user_id="+user.id+" and site_id=2";
+  } else {
+    
+    // udpate records
+    if (userId) {
+      let user = (await Users.getUserByIdSQL(userId))[0]
+      if (user) {
+        let userSiteLinksQuery = "select id, user_id, usertype_id from user_site_links where user_id="+user.id+" and site_id=2"
+        let updateQuery = "update user_site_links set usertype_id=20 where user_id="+user.id+" and site_id=2";
 
-      let response = await Users.getMysqlProduction(updateQuery)
+        let userSiteLinks = (await Users.getMysqlProduction(userSiteLinksQuery))[0]
+        let response = await Users.getMysqlProduction(updateQuery)
 
-      // res.json(response);
-      if (response) {
-        res.json({
-          status: true,
-          message: "User access type udpated"
-        });
+        // res.json(response);
+        if (response) {
+
+          // add record in users options 
+          let getDeletedPreference = (await Users.getMysqlProduction("select * from user_options where user_id="+userId+" and option_key='deleted'"))[0]
+
+          console.log(getDeletedPreference);
+
+          if (!getDeletedPreference) {
+            // add deleted record
+            Users.getMysqlProduction(`INSERT INTO user_options (user_id,option_key,option_value) values (${userId},'deleted','${userSiteLinks.usertype_id}')`)
+          } else {
+            // update deleted record
+            Users.getMysqlProduction(`update user_options set option_value='${userSiteLinks.usertype_id}' where user_id=${userId} and option_key='deleted'`)
+          }
+
+          res.json({
+            status: true,
+            message: "User access type udpated"
+          });
+        } else {
+          res.json({
+            status: false,
+            message: "Invalid user ID"
+          });
+        }
+
       } else {
         res.json({
           status: false,
-          message: "Invalid user ID"
+          message: "User does not exist"
         });
       }
-
     } else {
       res.json({
         status: false,
-        message: "User does not exist"
+        message: "Invalid user ID"
       });
     }
-  } else {
-    res.json({
-      status: false,
-      message: "Invalid user ID"
-    });
   }
+  
 
   // res.json({});
 
