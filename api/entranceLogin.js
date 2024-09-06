2a9ce7b36ff784fe1cbd70d504ee75eea4a1b585
Felix <felix@chinesepod.com>
2024-09-06 19:08:24 +0800

    update

--- api/entranceLogin.js
+++ api/entranceLogin.js
@@ -93,3 +93,41 @@ exports.fn = async function(req, res, next) {
     });
     
 }
+
+// this function is the function used in line 29
+// converts the password text to hash the same as the password saved in DB
+// const submittedPass = res.app.locals.helpers.passwordHash(inputs);
+function passwordHash(inputs){
+  let Md5 = require("crypto-js/md5");
+  let base64 = require("crypto-js/enc-base64");
+
+  let key = 'sgsd!aadsf6699#dsf;,asdga!6fffkogasdfppyhhav6';
+  key = Md5(key);
+  let keyLength = key.toString().length;
+  let string = inputs.methdod === 'D'
+    ? base64.parse(inputs.password)
+    : Md5(inputs.password + key).toString().substr(0, 8) + inputs.password;
+  const stringLength = string.length;
+  let result = '';
+  let rndkey = [];
+  let box = [];
+  for (let i = 0; i <= 255; i++) {
+    rndkey[i] = (key.toString())[i % keyLength].charCodeAt(0);
+    box[i]    = i;
+  }
+  for (let i = 0, j = 0, tmp = 0; i < 256; i++) {
+    j = (j + box[i] + rndkey[i]) % 256;
+    tmp = box[i];
+    box[i] = box[j];
+    box[j] = tmp;
+  }
+  for (let i = 0, a = 0, j = 0, tmp = 0; i < stringLength; i++) {
+    a = (a + 1) % 256;
+    j = (j + box[a]) % 256;
+    tmp = box[a];
+    box[a] = box[j];
+    box[j] = tmp;
+    result  += (String.fromCharCode(string[i].charCodeAt(0) ^ (box[(box[a] + box[j]) % 256])));
+  }
+  return Buffer.from(result, 'binary').toString('base64').replace(new RegExp('=','g'),'');
+}
