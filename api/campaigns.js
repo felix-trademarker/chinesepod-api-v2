03724f488cfb903a2b35311ff6d77c6c64986288
Felix <felix@chinesepod.com>
2024-10-01 15:13:43 +0800

    udpates

--- api/campaigns.js
+++ api/campaigns.js
@@ -4,7 +4,7 @@ exports.fn = async function(req, res, next) {
 
   let campaigns = await Users.getMysqlProduction(`
             SELECT DISTINCT(option_value) as campaign FROM user_options
-            where option_key='campaignid'
+            where option_key='campaignid' and last_update > '2024-01-01'
             order by last_update DESC
             `)
 
